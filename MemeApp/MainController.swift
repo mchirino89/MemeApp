fe2b701571ad1207dd738d35e5b35fe8c8904769
Mauricio Chirino <mauricio@3codegeeks.com>
2017-07-03 12:37:48 -0400

    - Added center placeholder with init instructions in case of no image is added yet. - Added animation for image removal and placeholder reappearance. - Disable action buttons as soon as image is removed.

--- MemeApp/MainController.swift
+++ MemeApp/MainController.swift
@@ -15,10 +15,12 @@ class MainController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var photoButton: UIBarButtonItem!
     @IBOutlet weak var albumButton: UIBarButtonItem!
     @IBOutlet weak var memeImage: UIImageView!
+    @IBOutlet weak var placeholderTextView: UITextView!
     let photoPicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        memeImage.autoresizingMask = UIViewAutoresizing.flexibleHeight
         photoPicker.delegate = self
         photoButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
@@ -28,10 +30,22 @@ class MainController: UIViewController, UIImagePickerControllerDelegate, UINavig
         print("Low memory in device")
     }
     
+    private func getImageFromSource(isCameraImage: Bool) {
+        photoPicker.sourceType = isCameraImage ? .camera : .photoLibrary
+        present(photoPicker, animated: true, completion: nil)
+    }
+    
+    private func enableActions(enable: Bool) {
+        shareButton.isEnabled = enable
+        deleteButton.isEnabled = enable
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        
         if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            memeImage.alpha = 1
+            placeholderTextView.alpha = 0
             memeImage.image = image
+            enableActions(enable: true)
         }
         dismiss(animated: true, completion: nil)
     }
@@ -39,16 +53,19 @@ class MainController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
-    
-    private func getImageFromSource(isCameraImage: Bool) {
-        photoPicker.sourceType = isCameraImage ? .camera : .photoLibrary
-        present(photoPicker, animated: true, completion: nil)
-    }
 
     @IBAction func shareMemeAction(_ sender: Any) {
+        print("Sharing image")
     }
     
     @IBAction func deleteMemeAction(_ sender: Any) {
+        UIView.animate(withDuration: 0.35, animations: {
+            self.enableActions(enable: false)
+            self.memeImage.alpha = 0
+            self.placeholderTextView.alpha = 1
+        }, completion: { _ in
+            self.memeImage.image = nil
+        })
     }
     
     @IBAction func takePhotoAction(_ sender: Any) {
