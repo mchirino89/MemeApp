67b4d6d99d4c3c1ea3ce951a3d5984850ec85480
Mauricio Chirino <mauricio@3codegeeks.com>
2017-07-03 00:00:55 -0400

    Validation for buttons' actions and added image from picker

--- MemeApp/MainController.swift
+++ MemeApp/MainController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class MainController: UIViewController {
+class MainController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    @IBOutlet weak var photoButton: UIBarButtonItem!
+    @IBOutlet weak var albumButton: UIBarButtonItem!
+    @IBOutlet weak var memeImage: UIImageView!
+    let photoPicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        photoPicker.delegate = self
+        photoButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         print("Low memory in device")
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            memeImage.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func getImageFromSource(isCameraImage: Bool) {
+        photoPicker.sourceType = isCameraImage ? .camera : .photoLibrary
+        present(photoPicker, animated: true, completion: nil)
+    }
 
-
+    @IBAction func shareMemeAction(_ sender: Any) {
+    }
+    
+    @IBAction func deleteMemeAction(_ sender: Any) {
+    }
+    
+    @IBAction func takePhotoAction(_ sender: Any) {
+        getImageFromSource(isCameraImage: true)
+    }
+    
+    @IBAction func pickPhotoAction(_ sender: Any) {
+        getImageFromSource(isCameraImage: false)
+    }
 }
-
